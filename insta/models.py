ee98b44dccff3c436a62c47e333e40d40660e516
yontey <muasajohn01@gmail.com>
2019-05-17 11:44:54 +0300

    Created the image model

--- insta/models.py
+++ insta/models.py
@@ -1,1 +1,9 @@
-from django.db import mode
+from django.db import models
+
+class Image(models.Model):
+    image = models.ImageField(uploadto='images/')
+    image_name = models.CharField(max_length=20)
+    image_caption = models.TextField(max_length=150)
+    likes = models.IntegerField(max_length=20)
+    comments = models.CharField(max_length=200)
+    
