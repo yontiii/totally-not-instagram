d3f33516857a793bc9bfba9ef7b4b5e16dc371e8
yontey <muasajohn01@gmail.com>
2019-05-17 12:20:00 +0300

    Run first migration and updated database

--- insta/models.py
+++ insta/models.py
@@ -2,14 +2,31 @@ from django.db import models
 
 
 class Profile(models.Model):
-    profile_photo = models.ImageField(uploadto='profiles/')
+    profile_photo = models.ImageField(upload_to='profiles/',default='default.jpg')
     bio = models.TextField(max_length=500)
+    
+    def __str__(self):
+        return self.bio
 
 class Image(models.Model):
-    image = models.ImageField(uploadto='images/')
+    image = models.ImageField(upload_to='images/')
     image_name = models.CharField(max_length=20)
     image_caption = models.TextField(max_length=150)
-    likes = models.IntegerField(max_length=20)
+    likes = models.IntegerField()
     comments = models.CharField(max_length=200)
     profile = models.ForeignKey(Profile)
+    
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()
+        
+    @classmethod
+    def update_image(cls, id):
+        images = cls.objects.filter(id=id).update(id=id)
+        return images
+    
+    def __str__(self):
+        return self.image_name
     
