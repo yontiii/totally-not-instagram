8456172f96fd2f8d77d47f7477fab0a020957f03
yontey <muasajohn01@gmail.com>
2019-05-17 15:13:16 +0300

    Added registration templates

--- instagram/urls.py
+++ instagram/urls.py
@@ -20,8 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('insta.urls')),
+    url(r'',include('insta.urls','registration.backends.simple.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
