9a401484e9f3ca79d2ae66c7c8d2027aec88ab7b
yontey <muasajohn01@gmail.com>
2019-05-17 11:48:18 +0300

    Created the profile model

--- insta/models.py
+++ insta/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(uploadto='profiles/')
+    bio = models.TextField(max_length=500)
+
 class Image(models.Model):
     image = models.ImageField(uploadto='images/')
     image_name = models.CharField(max_length=20)
     image_caption = models.TextField(max_length=150)
     likes = models.IntegerField(max_length=20)
     comments = models.CharField(max_length=200)
+    profile = models.ForeignKey(Profile)
     
